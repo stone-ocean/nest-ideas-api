4311ed7c412af874946d823f3883e25bc7602185
stone-ocean <>
2019-04-07 15:49:34 -0700

    switch nodemon to tsc-watch

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const port = process.env.PORT || 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.enableCors({
     origin: [
       'http://localhost:4200', // angular
@@ -15,7 +16,9 @@ async function bootstrap() {
       'http://localhost:8081', // react-native
     ],
   })
+
   await app.listen(port)
+
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap')
 }
 
